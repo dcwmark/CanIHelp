3119225d6f681da1b77bb628ea6cfda6f87f726d
Entaro Adun <dcwmark@gmail.com>
2021-08-21 21:43:28 -0400

    start up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createTheme,
+  ThemeProvider,
+  Container,
+  CssBaseline,
+} from '@material-ui/core';
+
+import GIRecorder from './components/gi_recorder';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#008080',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'FootLight',
+      'Georgia',
+      'Bradley Hand',
+      'Papyrus',
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={ theme }>
+        <CssBaseline />
+        <Container>
+          <GIRecorder />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/gi_recorder.js
@@ -0,0 +1,20 @@
+import {
+    Container,
+    Typography,
+  } from '@material-ui/core';
+
+  const gi_recorder = () => {
+    return (
+      <Container>
+        <Typography
+            variant="h4"
+            align="center"
+            color="primary"
+          >
+            GI Recorder
+          </Typography>
+      </Container>
+    );
+  }
+
+  export default gi_recorder;
